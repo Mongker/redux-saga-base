3a68df9fa9ba27d07bce199287ac619b4d6a0f7a
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-09-01 23:57:30 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Main from "./components/Main";
-
-// UI ANT DESIGN
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-
-// Redux
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-
-// Redux-Saga
-import createSagaMiddleware from "redux-saga";
-import rootSaga from "./sagas/rootSaga";
-
-// Reducers
-import allReducers from "./reducers";
-import MainContainer from "./components/MainContainer";
-
-// Middleware
-const sagaMiddleware = createSagaMiddleware();
-
-// Store
-let store = createStore(allReducers, applyMiddleware(sagaMiddleware));
-
-sagaMiddleware.run(rootSaga);
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <MainContainer />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  rootElement
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import MainReducers from "./MainReducers";
-
-const allReducers = combineReducers({
-  MainReducers
-  // Nếu có nhiều reducers thì viết thêm ở dướidưới
-});
-export default allReducers;
